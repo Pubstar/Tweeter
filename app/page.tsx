5aa8c594f8c3ea5c0ae7df17acc61eabd2732533
Pieter Hoek <pieterhoek@msn.com>
2024-08-26 21:56:25 +0200

    Change font

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 import CreateTweet from './Components/createTweet';
 import LoadTweets from './Components/loadTweets';
 import Nav from "./Components/Nav";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="bg-[#A1C349] h-screen">
-
-      <Nav />
-      <CreateTweet />
-      <LoadTweets />
-    </main>
+      <main className="bg-[#A1C349] h-screen">
+        <Nav />
+        <CreateTweet />
+        <LoadTweets />
+      </main>
   );
 }
