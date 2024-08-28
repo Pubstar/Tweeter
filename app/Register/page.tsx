31651a7ed369973466b2ee785ade9697c3c916ad
Pieter Hoek <pieterhoek@msn.com>
2024-08-28 21:29:00 +0200

    Working on user registration

--- app/Register/page.tsx
+++ app/Register/page.tsx
@@ -4,22 +4,28 @@ import React, { useState } from 'react'
 import Nav from '../Components/Nav'
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+interface User {
+  email: string,
+  password: string
+}
 
-/* const createUser(email, password) {
+
+function createUser(email: string, password: string): User {
   const auth = getAuth();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed up 
       const user = userCredential.user;
-      // ...
+      console.log(user, email,password);
+      return user;
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      // ..
+      console.log(error);
     });
-} */
-
+    return {email,password};
+}
 
 
 function page() {
@@ -27,16 +33,23 @@ function page() {
   const [username, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const submitForm = (e) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+  
+    createUser(username,password);
+  }  
+
   return (
     <div className="bg-[#A1C349] h-screen">
       <Nav />
       <div className='flex justify-center mt-24'>
-        <form action="" className='flex flex-col w-80 gap-4 border-2 border-[#243010] rounded-xl items-center p-8'>
+        <form action="" onSubmit={submitForm} className='flex flex-col w-80 gap-4 border-2 border-[#243010] rounded-xl items-center p-8'>
           <label htmlFor="email">Email: </label>
           <input onChange={event => setEmail(event.target.value)} className='rounded-xl' type="email" name="email" id="email" />
           <label htmlFor="password">Password: </label>
           <input onChange={event => setPassword(event.target.value)} className='rounded-xl' type="password" name="password" id="password" />
-          <button className='border-2 border-[#243010] mt-2 bg-[#243010] text-[#CAD593] font-bold rounded-xl w-44'>Create account</button>
+          <button type='submit' className='border-2 border-[#243010] mt-2 bg-[#243010] text-[#CAD593] font-bold rounded-xl w-44'>Create account</button>
         </form>
       </div>
     </div>
