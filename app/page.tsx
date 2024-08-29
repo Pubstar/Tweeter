12c30661197490b7f03a5761808ba2b9af4280ca
Pieter Hoek <pieterhoek@msn.com>
2024-08-29 21:54:27 +0200

    Various bug fixes and improvements

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@
 import CreateTweet from './Components/createTweet';
 import LoadTweets from './Components/loadTweets';
 import Nav from "./Components/Nav";
+import dbConnect from './db';
 import {auth} from './firebase';
 
 export default function Home() {
+  //dbConnect(); Need to connect the db and add tweets
   return (
       <main className="bg-[#A1C349] h-screen">
         <Nav />
