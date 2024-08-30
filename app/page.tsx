61fcc604ec24b8559bf4ba65c103897e6e92ba2a
Pieter Hoek <pieterhoek@msn.com>
2024-08-31 01:27:20 +0200

    Fix errors

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import Nav from "./Components/Nav";
 import {auth} from './firebase';
 
 export default function Home() {
-  const MONGODB_URI = process.env.MONGODB_URI
+/*   const MONGODB_URI = process.env.MONGODB_URI
 
-  mongoose.connect(MONGODB_URI)
+  mongoose.connect(MONGODB_URI) */
   return (
       <main className="bg-[#A1C349] h-screen">
         <Nav />
