cf3fdab89642d7da28204049b3814ed7b8c344d0
Pieter Hoek <pieterhoek@msn.com>
2024-09-05 21:20:39 +0200

    Display own tweets on profile

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Nav from '../Components/Nav'
 import { auth } from '../firebase'
 import { useRouter } from 'next/navigation';
+import LoadTweets from '../Components/LoadTweets';
 
 function Page() {
 
@@ -18,10 +19,12 @@ const handleSignOut = () => {
         <Nav />
         <div className='flex flex-col items-center'>
           <h1 className='my-16 font-bold text-5xl text-[#243010]'>Profile</h1>
-          <ul>
-            <li className='font-bold text-[#243010]'>Email: {auth.currentUser?.email}</li>
-          </ul>
+          <span className='font-bold text-[#243010]'>Email: {auth.currentUser?.email}</span>
           <span onClick={handleSignOut} className='mt-20 font-bold hover:cursor-pointer text-[#243010] border-2 border-[#243010] py-2 px-8'>Logout</span>
+          <div className='flex flex-col items-center'>
+            <h2 className='my-16 font-bold text-4xl text-[#243010]'>Tweets by you</h2>
+            < LoadTweets filter='self' />
+          </div>
         </div>
     </div>
   )
