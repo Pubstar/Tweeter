644bf1c28291575a16b9e2f1cdb8430a97ab0db2
Pieter Hoek <pieterhoek@msn.com>
2024-09-09 20:20:40 +0200

    Add username to profile page

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -20,6 +20,7 @@ const handleSignOut = () => {
           <h1 className='my-16 font-bold text-5xl text-[#243010]'>Profile</h1>
           <ul>
             <li className='font-bold text-[#243010]'>Email: {auth.currentUser?.email}</li>
+            <li className='font-bold text-[#243010]'>Username: {auth.currentUser?.displayName}</li>
           </ul>
           <span onClick={handleSignOut} className='mt-20 font-bold hover:cursor-pointer text-[#243010] border-2 border-[#243010] py-2 px-8'>Logout</span>
         </div>
